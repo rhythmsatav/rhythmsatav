0a2aec551f5e478b173ea37b6103a3aefec4f109
rytmstv <rhythmsatav@gmail.com>
2022-08-05 15:52:30 -0400

    Update Quantium_Task1.py

--- Quantium_Task1.py
+++ Quantium_Task1.py
@@ -4,9 +4,26 @@ import dash
 
 #product, sales(qauntity*price), region
 
-df = pandas.read_csv('daily_sales_data_0.csv')
+df = pd.read_csv('C:\Users\rhyth\Quantium Virtual Experience\daily_sales_data_0.csv')
 
 #selecting only the pink morsel
 df = [df['product'].str.contains('pink morsel')]
+
+
+df['sales'] = df['quantity'] * df['price']
+
+
+#delete price column
+df.pop('price')
+df.pop('quantity')
+
+
+# rename quantity to sales
+
+# df.rename(columns={"quantity": "sales"}, inplace=True)
+
+
+#delete product column
+df.pop('product')
 
 print(df)
